32edf01b638cefbf0257870d97c548384f23893f
david.smith <david.smith210@nhs.net>
2023-04-03 14:21:12 +0100

    AWS CDK Typescript initial dev

---
+++ test/cdk-tsc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTsc from '../lib/cdk-tsc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-tsc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTsc.CdkTscStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
